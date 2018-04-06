bc6af53afe449ad5c2672ab0987510b320de9dca
avsingh999 <avkaransingh357@gmail.com>
2018-04-06 16:26:25 +0530

    update

--- app.js
+++ app.js
@@ -23,6 +23,7 @@
 		});
 
 
-		app.listen(6883,"localhost",function(){
-			console.log("movie search");
-		})
+var port = process.env.PORT || 3000;
+	app.listen(port, "0.0.0.0", function() {
+			console.log("Listening on Port 3000");
+	});
